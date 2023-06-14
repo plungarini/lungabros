0518d05cf50d4b84ca9accc00e14e4afa4787ee9
Pietro Lungarini <pietro@lungarini.it>
2023-06-14 23:41:26 +0200

    feat: Add course card component to section two on home page

---
+++ src/app/pages/home/components/section-two/components/course-card/course-card.component.ts
@@ -0,0 +1,38 @@
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    Input,
+    OnInit,
+} from '@angular/core';
+import { Course } from 'src/app/shared/models/course.model';
+
+@Component({
+  selector: 'app-course-card',
+  templateUrl: './course-card.component.html',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CourseCardComponent implements OnInit {
+  @Input('course') set setCourse(value: Course | undefined) {
+    if (!value) return;
+    this.course = value;
+    this.cdRef.detectChanges();
+  }
+
+  course: Course | undefined;
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  ngOnInit(): void {}
+
+  get isHighlighted(): boolean {
+    return false;
+  }
+}
