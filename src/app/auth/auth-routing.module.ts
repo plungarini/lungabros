a4676e84f85f5150640da299be8fb1b60e968b89
Pietro Lungarini <pietro@lungarini.it>
2023-06-14 23:38:44 +0200

    feat: add login component to auth module and update angular.json to prerender /auth/login route

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth.component';
+import { LoginComponent } from './components/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: AuthComponent,
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+    ]
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,17 +1,41 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { ActivatedRoute, NavigationStart, Router } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styles: [
-    `
-      :host {
-        display: block;
-      }
-    `
-  ],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styles: []
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit, OnDestroy {
+
+  routerSub: Subscription | undefined;
+
+  constructor(
+    private pageTitle: Title,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
+
+  ngOnInit(): void {
+    this.pageTitle.setTitle(`LUNGABROS | Login`);
+    this.routerSub = this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationStart),
+      ).subscribe(e => {
+        if (e instanceof NavigationStart) {
+          if (e.url.includes('login')) {
+            this.pageTitle.setTitle(`LUNGABROS | Admin Login`);
+          }
+        }
+      });
+    const returnUrl = this.route.snapshot.paramMap.get('returnUrl');
+    if (returnUrl) localStorage.setItem('returnUrl', returnUrl);
+  }
+
+  ngOnDestroy(): void {
+    this.routerSub?.unsubscribe();
+  }
 
 }
