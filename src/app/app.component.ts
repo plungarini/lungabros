736bb241480faeed2085588576e7b13c763eb74c
Pietro Lungarini <pietro@lungarini.it>
2023-06-14 22:46:51 +0200

    AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { NavigationStart, RouteConfigLoadEnd, RouteConfigLoadStart, Router } from '@angular/router';
+import { Subscription, filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,59 @@ import { Component } from '@angular/core';
   styles: []
 })
 export class AppComponent {
-  title = 'lungabros';
+  routerSub: Subscription | undefined;
+  isAdminArea = false;
+  showLoader = true;
+	firstStart = true;
+	
+	constructor(
+    private titleService: Title,
+    private router: Router,
+    private cdRef: ChangeDetectorRef
+  ) {
+    this.titleService.setTitle(`LUNGABROS`);
+
+    this.routerSub = this.router.events
+      .pipe(
+        filter(
+          (event) =>
+            event instanceof NavigationStart ||
+            event instanceof RouteConfigLoadStart ||
+            event instanceof RouteConfigLoadEnd
+        )
+      )
+      .subscribe((e) => {
+        if (
+          !this.firstStart &&
+          (e instanceof RouteConfigLoadStart || e instanceof RouteConfigLoadEnd)
+        ) {
+          let asyncLoadCount = 0;
+          if (e instanceof RouteConfigLoadStart) {
+            asyncLoadCount++;
+          } else if (e instanceof RouteConfigLoadEnd) {
+            asyncLoadCount > 0 && asyncLoadCount--;
+          }
+          this.showLoader = !!asyncLoadCount;
+          this.cdRef.detectChanges();
+        }
+
+        if (e instanceof NavigationStart) {
+          if (e.url === '/') {
+            this.titleService.setTitle(`LUNGABROS`);
+          } else if (e.url.includes('about')) {
+            this.titleService.setTitle(`LUNGABROS | About us`);
+          } else if (e.url.includes('contact')) {
+            this.titleService.setTitle(`LUNGABROS | Contattaci`);
+          }
+
+          if (e.url.includes('admin') || e.url.includes('login')) {
+            this.isAdminArea = true;
+            this.cdRef.detectChanges();
+          } else {
+            this.isAdminArea = false;
+            this.cdRef.detectChanges();
+          }
+        }
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,60 @@
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { ScreenTrackingService, UserTrackingService, getAnalytics, provideAnalytics } from '@angular/fire/analytics';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFunctions, provideFunctions } from '@angular/fire/functions';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { ImgixAngularModule } from '@imgix/angular';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideFunctions,getFunctions } from '@angular/fire/functions';
-import { provideStorage,getStorage } from '@angular/fire/storage';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+		AppComponent,
+		FooterComponent,
+		NavbarComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
+		BrowserModule,
+		BrowserAnimationsModule,
+
+		AppRoutingModule,
+		
+		// PWA
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
-    }),
+		}),
+		
+		// Firebase
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideAnalytics(() => getAnalytics()),
     provideAuth(() => getAuth()),
     provideFirestore(() => getFirestore()),
     provideFunctions(() => getFunctions()),
-    provideStorage(() => getStorage())
+		provideStorage(() => getStorage()),
+		
+		// Dependencies
+		ImgixAngularModule.forRoot({
+      domain: 'lungabros.imgix.net',
+      defaultImgixParams: {
+        auto: 'format,compress',
+      },
+    }),
   ],
   providers: [
-    ScreenTrackingService,UserTrackingService
+		ScreenTrackingService,
+		UserTrackingService
   ],
   bootstrap: [AppComponent]
 })
