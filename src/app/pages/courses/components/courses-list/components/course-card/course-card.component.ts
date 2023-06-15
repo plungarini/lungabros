394e602ee3975705318384e8cb25034da5546dcb
Pietro Lungarini <pietro@lungarini.it>
2023-06-15 15:24:53 +0200

    feat: Add course card component for displaying course information in courses list page

---
+++ src/app/pages/courses/components/courses-list/components/course-card/course-card.component.ts
@@ -0,0 +1,42 @@
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    Input,
+    OnInit,
+} from '@angular/core';
+import { Course } from '../../../../../../shared/models/course.model';
+
+@Component({
+  selector: 'app-course-card',
+  templateUrl: './course-card.component.html',
+  styles: [
+    `
+      :host {
+        display: block;
+      }
+    `,
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class CourseCardComponent implements OnInit {
+  @Input('course') set setCourse(value: Course | undefined) {
+    if (!value) return;
+    this.course = {
+      ...value,
+      bgImg: value.bgImg || 'stock/header.jpeg',
+      gallery: value.gallery.map((i) => i || 'stock/header.jpeg'),
+    };
+    this.cdRef.detectChanges();
+  }
+
+  course: Course | undefined;
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  ngOnInit(): void {}
+
+  get isHighlighted(): boolean {
+    return this.course?.category.includes('main') ? true : false;
+  }
+}
