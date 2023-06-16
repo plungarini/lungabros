b0266d538264f5efbb2c45a57f1f9648d4d3aafe
Pietro Lungarini <pietro@lungarini.it>
2023-06-16 16:25:45 +0200

    feat: Add sitemap generation and update functionality

--- functions/src/courseRoutes/index.ts
+++ functions/src/courseRoutes/index.ts
@@ -4,7 +4,8 @@ import { db } from '..';
 
 const owner = 'plungarini';
 const repo = 'lungabros';
-const path = 'routes.txt';
+const routesFilePath = 'routes.txt';
+const sitemapPath = 'src/assets/sitemap.xml';
 const token = process.env.GITHUB_TOKEN;
 
 export const updateRoutes = async () => {
@@ -22,7 +23,7 @@ export const updateRoutes = async () => {
 
   warn(`Found ${docs.length} routes`, fileContent);
 
-  const requestUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+  const requestUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${routesFilePath}`;
 
   try {
     // Get current file information
@@ -33,7 +34,15 @@ export const updateRoutes = async () => {
     });
 
     const currentFile = response.data;
-    const sha = currentFile.sha;
+		const sha = currentFile.sha;
+		const currentContent = Buffer.from(currentFile.content, 'base64').toString();
+		
+		if (currentContent === fileContent) {
+      warn('The content is already up to date. Skipping the update.');
+      return;
+		}
+		
+		warn('Content is different, updating file on GitHub...');
 
     // Update file with new content and sha
     const requestBody = {
@@ -49,9 +58,81 @@ export const updateRoutes = async () => {
       },
     });
 
-    warn('Routes updated successfully');
+		warn('Routes updated successfully, updating sitemap...');
+		await updateSitemap([...routes]);
   } catch (err) {
     const message = (err as any)?.response?.data?.message;
     throw message ? new Error(message) : err;
   }
 };
+
+const defaultRoutes = [
+	{ url: 'https://lungabros.it/', priority: 0 },
+	{ url: 'https://lungabros.it/contact', priority: 0 },
+	{ url: 'https://lungabros.it/articles/importanza-della-sicurezza', priority: 0 },
+	{ url: 'https://lungabros.it/articles/torchbearers', priority: 0 },
+	{ url: 'https://lungabros.it/about', priority: 0 },
+	{ url: 'https://lungabros.it/about/pietro-lungarini', priority: 0 },
+	{ url: 'https://lungabros.it/about/samuele-lungarini', priority: 0 },
+];
+
+const updateSitemap = async (coursesRoutes: Array<string>) => {
+	const newSitemapRoutes = [
+		...defaultRoutes,
+		...coursesRoutes
+			.map((r) => ({ url: `https://lungabros.it/${r}`, priority: 0.7 }))
+	];
+
+	let sitemapXml = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n`;
+	newSitemapRoutes.forEach(({ url, priority }) => {
+		sitemapXml += `\t<url>\n`;
+		sitemapXml += `\t\t<loc>${url}</loc>\n`;
+		sitemapXml += `\t\t<priority>${priority}</priority>\n`;
+		sitemapXml += `\t\t<changefreq>weekly</changefreq>\n`;
+		sitemapXml += `\t</url>\n`;
+	});
+
+	sitemapXml += `</urlset>`;
+	
+	warn('Generated sitemapXml => ', sitemapXml);
+
+  const requestUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${sitemapPath}`;
+
+	try {
+		// Get current file information
+		const response = await axios.get(requestUrl, {
+			headers: {
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const currentFile = response.data;
+		const sha = currentFile.sha;
+		const currentContent = Buffer.from(currentFile.content, 'base64').toString();
+		
+		if (currentContent === sitemapXml) {
+			warn('The content is already up to date. Skipping the update.');
+			return;
+		}
+		
+		warn('Content is different, updating file on GitHub...');
+
+		// Update file with new content and sha
+		const requestBody = {
+			message: '[FIRESTORE EVENT] Update sitemap file',
+			content: Buffer.from(sitemapXml).toString('base64'),
+			sha: sha,
+		};
+
+		await axios.put(requestUrl, requestBody, {
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+		warn('Sitemap updated successfully');
+	} catch (err) {
+		const message = (err as any)?.response?.data?.message;
+    throw message ? new Error(message) : err;
+	}
+}
